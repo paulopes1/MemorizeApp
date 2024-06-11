a44d49b2535b3b61b9930d66ae0484499766916f
paulopes1 <paulolopesplj@gmail.com>
2024-06-11 19:04:45 -0300

    Initial Commit

---
+++ MemorizeOfficial/MemorizeOfficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeOfficialApp.swift
+//  MemorizeOfficial
+//
+//  Created by Paulo Lopes on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeOfficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
